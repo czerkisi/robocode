2dca440a43342b2b464fca721176762dada715de
naveedhaq02 <naveedhaq@tamu.edu>
2024-07-08 22:20:39 -0400

    spinny

--- JPMC/Roboken2.java
+++ JPMC/Roboken2.java
@@ -1,29 +1,53 @@
 package JPMC;
 
 import robocode.Robot;
+import robocode.HitRobotEvent;
+import robocode.ScannedRobotEvent;
 
+import java.awt.*;
+
+/**
+ * SpinBot - a sample robot by Mathew Nelson.
+ * <p>
+ * Moves in a circle, firing hard when an enemy is detected.
+ *
+ * @auther Mathew A. Nelson (original)
+ * @auther Flemming N. Larsen (contributor)
+ */
 public class Roboken2 extends Robot {
 
+    /**
+     * SpinBot's run method - Circle
+     */
     public void run() {
-        // Loop to continuously perform the figure-eight movement
+        // Set colors
+        setColors(Color.blue, Color.blue, Color.black, Color.yellow, Color.white);
+
+        // Loop forever
         while (true) {
-            moveInFigureEight();
+            // Turn the robot and move ahead indefinitely
+            turnRight(10000);
+            ahead(10000);
         }
     }
 
-    private void moveInFigureEight() {
-        // Move in one loop of the figure-eight
-        for (int i = 0; i < 2; i++) {
-            // Move in a circular pattern
-            for (int j = 0; j < 36; j++) {
-                ahead(10);
-                turnRight(10);
-            }
-            // Reverse the direction for the second loop
-            for (int j = 0; j < 36; j++) {
-                ahead(10);
-                turnLeft(10);
-            }
+    /**
+     * onScannedRobot: Fire hard!
+     */
+    public void onScannedRobot(ScannedRobotEvent e) {
+        fire(3);
+    }
+
+    /**
+     * onHitRobot:  If it's our fault, we'll stop turning and moving,
+     * so we need to turn again to keep spinning.
+     */
+    public void onHitRobot(HitRobotEvent e) {
+        if (e.getBearing() > -10 && e.getBearing() < 10) {
+            fire(3);
+        }
+        if (e.isMyFault()) {
+            turnRight(10);
         }
     }
 }
